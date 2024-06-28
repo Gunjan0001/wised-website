a32001f9b848bc4d697e5a88b718fbde4a300c34
gunjan <gunjandora595@gmail.com>
2024-06-28 09:51:47 +0530

    code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,14 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        xxs: '10px',
-        xs: '12px',
-        sm: '14px',
-        base: '16px',
-        md: '18px',
-        lg: '20px',
-        "12": '24px',
-        "2xl": '72px',
+        xxs: "10px",
+        xs: "12px",
+        sm: "14px",
+        base: "16px",
+        md: "18px",
+        lg: "20px",
+        xl: "24px",
+        "2xl": "72px",
       },
 
       colors: {
@@ -27,46 +27,6 @@ module.exports = {
           400: "#EEEFEF",
         },
       },
-
-      screens: {
-        sm: '640px', // Small screens (min-width: 640px)
-        md: '768px', // Medium screens (min-width: 768px)
-        lg: '1024px', // Large screens (min-width: 1024px)
-        xl: '1280px', // Extra large screens (min-width: 1280px)
-        '2xl': '1536px', // 2XL screens (min-width: 1536px)
-      },
-
-
-      // fontSize: {
-      //   base: {
-      //     sm: '17px', // min-width: 640px
-      //     md: '18px', // min-width: 768px
-      //     lg: '19px', // min-width: 1024px
-      //     xl: '20px', // min-width: 1280px
-      //     '2xl': '21px', // min-width: 1536px
-      //   },
-      //   lg: {
-      //     sm: '21px',
-      //     md: '22px',
-      //     lg: '23px',
-      //     xl: '24px',
-      //     '2xl': '25px',
-      //   },
-      //   xl: {
-      //     sm: '25px',
-      //     md: '26px',
-      //     lg: '27px',
-      //     xl: '28px',
-      //     '2xl': '29px',
-      //   },
-      //   "2xl": {
-      //     sm: '80px',
-      //     md: '88px',
-      //     lg: '96px',
-      //     xl: '104px',
-      //     '2xl': '112px',
-      //   },
-      // },
     },
   },
 };
