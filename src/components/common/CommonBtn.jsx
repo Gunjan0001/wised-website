cb527a62063c94d020b1ce248329bfca2f344621
Mj-Saini <manojsaini97285@gmail.com>
2024-06-28 09:26:22 +0530

    add logo and icon and create auth folder

---
+++ src/components/common/CommonBtn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CommonBtn = () => {
+  return (
+    <div>
+      <button className="text-base font-normal text-white py-2 px-8 bg-primary">
+        Sign in
+      </button>
+    </div>
+  );
+};
+
+export default CommonBtn;
